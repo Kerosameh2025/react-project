909794e8958538c682ebb990331c3e8ed97fad80
kerolos <kero.sameh.2024@gmail.com>
2025-04-08 14:15:55 +0200

    git init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import Home from "./components/pages/Home.jsx";
+import MyFavorites from "./components/pages/MyFavorites.jsx";
+
+function App() {
+  return (
+    <>
+      <MyFavorites />
+      {/* <Home></Home> */}
+    </>
+  );
+}
+
+export default App;
